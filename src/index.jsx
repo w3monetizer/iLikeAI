42cd8f020f63175c72fab2892f71964f71f93c5b
CodeArtistryCode <realcodeartistry@gmail.com>
2020-07-21 09:15:03 -0400

    Init

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ListPage from "./pages/ListPage";
+import HomePage from "./pages/HomePage";
+// import Loading from "./components/shared/Loading";
+// import SignIn from "./components/SignIn";
+// import useAuth from "./hooks/useAuth";
+
+function App() {
+  return "app";
+}
+
+function AuthApp({ user }) {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/:listId" component={ListPage} />
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+function UnAuthApp() {
+  return "unauth";
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
