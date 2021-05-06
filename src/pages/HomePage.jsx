21a29432ef5ec546b564eeb6ab8b3f478bebf2e8
stefian <v_ianta@yahoo.com>
2021-05-06 18:26:38 -0300

    SI @1620336398682 ms + import {UserContext} from '../index';

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import CreateList from "../components/CreateList";
 import Lists from "../components/Lists";
 import Layout from "../components/shared/Layout";
-import UserContext from '../index';
+import {UserContext} from '../index';
 
 function HomePage() {
   const user = React.useContext(UserContext);
