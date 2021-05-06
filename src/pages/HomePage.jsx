d124a1a6942fdecc7e64c164687c1900555177e4
stefian <v_ianta@yahoo.com>
2021-05-06 18:25:41 -0300

    SI @1620336341705 ms +   const user = React.useContext(UserContext);   console.log(user);

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import CreateList from "../components/CreateList";
 import Lists from "../components/Lists";
 import Layout from "../components/shared/Layout";
+import UserContext from '../index';
 
 function HomePage() {
+  const user = React.useContext(UserContext);
+  console.log(user);
+
   return (
     <Layout>
       <CreateList />
