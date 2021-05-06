a81646d404c18297e0968bd40a1853bd0ffdf36c
stefian <v_ianta@yahoo.com>
2021-05-06 14:18:23 -0300

    SI @1620321502978 ms +                 onClick={db.signInWithGoogle}

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -21,7 +21,7 @@ function SignIn() {
             </p>
             <div className="flex justify-center">
               <button
-                // onClick={db.signInWithGoogle}
+                onClick={db.signInWithGoogle}
                 className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
               >
                 Sign In With Google
