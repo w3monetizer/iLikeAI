3aa5c8f66484812b2b8d4a3587dadf07c8371fee
stefian <v_ianta@yahoo.com>
2021-05-06 15:34:07 -0300

    SI @1620326047010 ms +           Create Repo

--- src/components/CreateList.jsx
+++ src/components/CreateList.jsx
@@ -13,7 +13,7 @@ function CreateList() {
       <div className="lg:w-2/6 mx-auto md:w-1/2 bg-gray-800 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
         <input
           className="bg-gray-900 rounded border text-white border-gray-900 focus:outline-none focus:border-green-500 text-base px-4 py-2 mb-4"
-          placeholder="Add list name"
+          placeholder="Add repo name"
           type="text"
           name="name"
           required
@@ -26,13 +26,13 @@ function CreateList() {
         />
         <input
           className="bg-gray-900 rounded border text-white border-gray-900 focus:outline-none focus:border-green-500 text-base px-4 py-2 mb-4"
-          placeholder="Add list name"
+          placeholder="Add repo name"
           type="file"
           name="image"
         />
         {/* display preview image */}
         <button className="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">
-          Create List
+          Create Repo
         </button>
         <p className="text-xs text-gray-600 mt-3">*List name required</p>
       </div>
