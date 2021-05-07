f667d4c0cbae1a8998dcbc9b475e10164009347a
stefian <v_ianta@yahoo.com>
2021-05-07 17:00:55 -0300

    SI @1620417655353 ms + function ListItem({ list }) {   const { id, name, description, image, users } = list;

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -56,27 +56,29 @@ function UserListCount({count}) {
   );
 }
 
-function ListItem() {
+function ListItem({ list }) {
+  const { id, name, description, image, users } = list;
+  
   return (
     <div className="lg:w-1/3 sm:w-1/2 p-4">
       {" "}
-      <Link to={`/id`}>
+      <Link to={`/${id}`}>
         <div className="flex relative">
           <img
             alt="gallery"
             className="absolute inset-0 w-full h-full object-cover object-center"
-            src={defaultImage}
+            src={image || defaultImage}
           />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
             <ul className="list-disc">
               <li className="tracking-widest text-sm title-font font-medium text-orange-500 mb-1">
-                Username + 2 others
+                {users[0].name} + {users.length > 1 && `+ ${users.length - 1} others`}
               </li>
             </ul>
             <h1 className="title-font text-lg font-medium text-white mb-3">
-              Name
+              {name}
             </h1>
-            <p className="leading-relaxed">Description</p>
+            <p className="leading-relaxed">{description}</p>
           </div>
         </div>
       </Link>
