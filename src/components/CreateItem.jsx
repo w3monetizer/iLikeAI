6fcf2656a89fe3481716003ca046793d2e2bc1da
stefian <v_ianta@yahoo.com>
2021-05-08 07:30:36 -0300

    SI @1620469836592 ms +       <form onSubmit={handleCreateItem}

--- src/components/CreateItem.jsx
+++ src/components/CreateItem.jsx
@@ -9,7 +9,8 @@ function CreateItem() {
 
   return (
     <>
-      <form className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:px-0">
+      <form onSubmit={handleCreateItem}
+        className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:px-0">
         <input
           className="flex-grow w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-green-500 text-base px-4 py-2 mr-4 mb-4 sm:mb-0"
           name="name"
