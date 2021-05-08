5b6d6ba71a468f381f5fe9461e57f64fe803104c
stefian <v_ianta@yahoo.com>
2021-05-08 07:26:34 -0300

    SI @1620469594113 ms +   const [submitting, setSubmitting] = React.useState('false')

--- src/components/CreateItem.jsx
+++ src/components/CreateItem.jsx
@@ -5,6 +5,7 @@ import React from "react";
 function CreateItem() {
   const [name, setName] = React.useState('')
   const [link, setLink] = React.useState('')
+  const [submitting, setSubmitting] = React.useState('false')
 
   return (
     <>
